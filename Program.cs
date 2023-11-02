7a9495af3f7a5a48e2719c3ff3610f5231225c5f
Alirezamohammadi-13 <149057897+Alirezamohammadi-13@users.noreply.github.com>
2023-11-02 16:53:58 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Alireza mohammadi");
+Console.WriteLine("factoriyel adad");
+
+
+Console.WriteLine(" factoriel adad chand?");
+
+int z = Convert.ToInt32(Console.ReadLine());
+int y = 1;
+for (int s = 1; s <= z; s++)
+{
+    y = y * s;
+
+}
+Console.WriteLine(y);
+Console.WriteLine("tamam ");
